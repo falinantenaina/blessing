9e197ad3a281ddfb2ad8bce45a1077b1619bb78e
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2026-02-08 18:57:55 +0300

    Add public inscription route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,18 +2,24 @@ import express from "express";
 import authRoutes from "./auth.routes.js";
 import financeRoutes from "./finance.routes.js";
 import horaireRoutes from "./horaire.routes.js";
+import inscriptionPubliqueRoutes from "./inscription-publique.route.js";
 import inscriptionRoutes from "./inscription.routes.js";
 import jourRoutes from "./jour.routes.js";
 import niveauRoutes from "./niveau.routes.js";
 import referenceRoutes from "./reference.routes.js";
 import salleRoutes from "./salle.routes.js";
 import userRoutes from "./user.routes.js";
-import vagueRoutes from "./vague.routes.js";
+import {
+  default as etudianRoutes,
+  default as vagueRoutes,
+} from "./vague.routes.js";
 
 const router = express.Router();
 
 // Montage des routes
 router.use("/auth", authRoutes);
+router.use("/inscriptions-publiques", inscriptionPubliqueRoutes);
+
 router.use("/users", userRoutes);
 router.use("/salles", salleRoutes);
 router.use("/vagues", vagueRoutes);
@@ -23,6 +29,7 @@ router.use("/finances", financeRoutes);
 router.use("/reference", referenceRoutes);
 router.use("/horaires", horaireRoutes);
 router.use("/jours", jourRoutes);
+router.use("/etudiants", etudianRoutes);
 
 // Route de test
 router.get("/health", (req, res) => {
